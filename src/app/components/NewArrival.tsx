9280cbecbf34105e8cd21efdd1e617a8edfe7d4f
unknown <fatmasaleh567@gmail.com>
2025-02-05 04:35:45 +0200

    add skeleton

--- src/app/components/NewArrival.tsx
+++ src/app/components/NewArrival.tsx
@@ -8,12 +8,14 @@ import SwiperSection from './SwipperSection';
 
 const NewArrival = () => {
     const [filteredProducts, setFilteredProducts] = useState<ProductData[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         getAllProducts();
     }, []);
 
     const getAllProducts = async () => {
+        setLoading(true);
         try {
             const result = await db
                 .select()
@@ -26,11 +28,34 @@ const NewArrival = () => {
             } catch (error) {
                 console.error("Error fetching products:", error);
             }
+            setLoading(false);
+            
         
     };
   return (
     <div className='mt-16 xl:mx-[100px] lg:mx-16 mx-5 pb-16 border-b '>
-        <SwiperSection title="New Arrival" product={filteredProducts} />
+        {loading?
+        (
+            <div>
+                 <h1 className='xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl font-bold uppercase text-center mb-10 tracking-tighter'>New Arrival</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            {[...Array(1)].map((_, index) => (
+                        <div key={index} className='w-64 place-self-center h-96 bg-[#F0EEED] animate-pulse rounded-xl sm:hidden'></div>
+                    ))}
+                    {[...Array(2)].map((_, index) => (
+                        <div key={index} className='w-64 h-96 bg-[#F0EEED] animate-pulse rounded-xl hidden sm:block md:hidden'></div>
+                    ))}
+                    {[...Array(4)].map((_, index) => (
+                        <div key={index} className='w-64 h-96 bg-[#F0EEED] animate-pulse rounded-xl hidden md:block'></div>
+                    ))}
+            </div>
+            </div>
+        ):(
+            
+            <SwiperSection title="New Arrival" product={filteredProducts} />
+        )
+        }
+        
     </div>
   )
 }
--- src/app/components/TopSelling.tsx
+++ src/app/components/TopSelling.tsx
@@ -9,12 +9,14 @@ import SwiperSection from './SwipperSection';
 
 const TopSelling = () => {
   const [filteredProducts, setFilteredProducts] = useState<ProductData[]>([]);
+  const [loading, setLoading] = useState(true);
   
       useEffect(() => {
           getAllProducts();
       }, []);
   
       const getAllProducts = async () => {
+        setLoading(true)
           try {
               const result = await db
                   .select()
@@ -27,11 +29,32 @@ const TopSelling = () => {
               } catch (error) {
                   console.error("Error fetching products:", error);
               }
+              setLoading(false)
           
       };
   return (
     <div className='mt-16 xl:mx-[100px] lg:mx-16 mx-5 '>
-        <SwiperSection title="Top Selling" product={filteredProducts} />
+         {loading?
+        (
+            <div>
+                 <h1 className='xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl font-bold uppercase text-center mb-10 tracking-tighter'>Top Selling</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            {[...Array(1)].map((_, index) => (
+                        <div key={index} className='w-64 place-self-center h-96 bg-[#F0EEED] animate-pulse rounded-xl sm:hidden'></div>
+                    ))}
+                    {[...Array(2)].map((_, index) => (
+                        <div key={index} className='w-64 h-96 bg-[#F0EEED] animate-pulse rounded-xl hidden sm:block md:hidden'></div>
+                    ))}
+                    {[...Array(4)].map((_, index) => (
+                        <div key={index} className='w-64 h-96 bg-[#F0EEED] animate-pulse rounded-xl hidden md:block'></div>
+                    ))}
+            </div>
+            </div>
+        ):(
+            
+            <SwiperSection title="Top Selling" product={filteredProducts} />
+        )
+        }
     </div>
   )
 }
