6bc9d018421f32cbc9d7f04f8100c23d72eaa6e1
unknown <fatmasaleh567@gmail.com>
2025-02-25 05:24:58 +0200

    add admin auth

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -1,19 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import Sidebar from "../components/Sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const { isSignedIn, user, isLoaded } = useUser();
+  const router = useRouter();
+  const [isAuthorized, setIsAuthorized] = useState(false);
 
-export default function layout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-        <SidebarProvider>
-        <Sidebar />
-        <main className="w-[100%] overflow-hidden">
-          <SidebarTrigger />
-          {children}
-        </main>
-      </SidebarProvider>
-        )
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (!isSignedIn) {
+      router.push("/sign-in");
+      return;
+    }
+
+    if (user?.publicMetadata?.role !== "admin") {
+      router.push("/");
+      return;
+    }
+
+    setIsAuthorized(true); 
+  }, [isSignedIn, user, isLoaded, router]);
+
+
+  if (!isLoaded || !isAuthorized) {
+    return null;
   }
+
+  return (
+    <SidebarProvider>
+      <Sidebar />
+      <main className="w-[100%] overflow-hidden">
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
